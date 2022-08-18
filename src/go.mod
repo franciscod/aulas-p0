3771d71479d68b6359f187afa68e4b6a59b101ed
Francisco Demartino <demartino.francisco@gmail.com>
2022-08-18 17:41:36 -0300

    hello map world

--- src/go.mod
+++ src/go.mod
@@ -2,4 +2,9 @@ module mapita
 
 go 1.16
 
-require github.com/tdewolff/canvas v0.0.0-20220627195642-6566432f4b20 // indirect
+require (
+	github.com/rustyoz/Mtransform v0.0.0-20190224104252-60c8c35a3681 // indirect
+	github.com/rustyoz/genericlexer v0.0.0-20190224115003-eb82fd2987bd // indirect
+	github.com/rustyoz/svg v0.0.0-20200706102315-fe1aeca2ba20
+	github.com/tdewolff/canvas v0.0.0-20220627195642-6566432f4b20
+)
--- src/main.go
+++ src/main.go
@@ -1,25 +1,86 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/tdewolff/canvas"
 	"github.com/tdewolff/canvas/renderers"
+
+	"github.com/rustyoz/svg"
 )
 
 func main() {
-	// Create new canvas of dimension 100x100 mm
-	c := canvas.New(100, 100)
-
-	// Create a canvas context used to keep drawing state
+	c := canvas.New(1000, 500)
 	ctx := canvas.NewContext(c)
 
-	// Create a triangle path from an SVG path and draw it to the canvas
-	triangle, err := canvas.ParseSVG("L60 0L30 60z")
+	ctx.SetStrokeWidth(10.0)
+
+	scale := 0.4
+	xmin := 400.0
+	ymin := 1300.0
+
+	ctx.SetView(canvas.Identity.Translate(0.0, 0.0).Scale(scale, -scale).Translate(-xmin, -ymin))
+
+	reader, err := os.Open("../data/mapa.svg")
+	doc, err := svg.ParseSvgFromReader(reader, "test", 1)
+
 	if err != nil {
 		panic(err)
 	}
-	ctx.SetFillColor(canvas.Mediumseagreen)
-	ctx.DrawPath(20, 20, triangle)
 
-	// Rasterize the canvas and write to a PNG file with 3.2 dots-per-mm (320x320 px)
-	renderers.Write("getting-started.png", c)
+	di, _ := doc.ParseDrawingInstructions()
+
+	done := false
+	for !done {
+		select {
+		case ins, ok := <-di:
+			if !ok {
+				done = true
+				break
+			}
+			if ins == nil {
+				continue
+			}
+			log.Println(ins.M)
+			switch ins.Kind {
+			case svg.MoveInstruction:
+				{
+					ctx.MoveTo(ins.M[0], ins.M[1])
+				}
+			case svg.CircleInstruction:
+				{
+					ctx.MoveTo(ins.M[0], ins.M[1])
+					ctx.Arc(*ins.Radius, *ins.Radius, 0, 0, 360)
+				}
+			case svg.CurveInstruction:
+				{
+					ctx.CubeTo(
+						ins.CurvePoints.C1[0],
+						ins.CurvePoints.C1[1],
+						ins.CurvePoints.C2[0],
+						ins.CurvePoints.C2[1],
+						ins.CurvePoints.T[0],
+						ins.CurvePoints.T[1],
+					)
+				}
+			case svg.LineInstruction:
+				{
+					ctx.LineTo(ins.M[0], ins.M[1])
+				}
+			case svg.PaintInstruction:
+				{
+					ctx.SetFillColor(canvas.Mediumseagreen)
+					ctx.SetStrokeColor(canvas.Mediumseagreen)
+					ctx.Stroke()
+					ctx.Fill()
+				}
+			default:
+				log.Println("wtf")
+				panic(ins.Kind)
+			}
+		}
+	}
+
+	renderers.Write("mapa.png", c)
 }
